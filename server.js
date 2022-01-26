ae866dc2592cb8fde15ab3537ee4bbf115f50941
Michelle Nijjar <michellenijjar@hotmail.com>
2022-01-25 20:15:44 -0500

    final commit

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const PORT = process.env.PORT || 3009;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/trackyourbudgettoday";
 
 const app = express();
 
